3eb1de0c5adffe22e7948cb60152bd022576f172
MOTIZ-dev <ajimotihamzat1@gmail.com>
2026-09-19 22:28:01 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1065,9 +1065,8 @@ def admin(nickname, user):
                         if n.get('id')==nid:
                             n['title']=request.form["notice_title"]; n['text']=request.form["notice_text"]; n['media_link']=request.form.get("media_link","")
                     set_setting("notices", json.dumps(NOTICES)); error = "<div class=success>Notice Updated</div>"
-                if "delete_notice" in request.form:
+                                if "delete_notice" in request.form:
                     nid = int(request.form["notice_id"])
-                    global NOTICES
                     NOTICES = [n for n in NOTICES if n.get('id')!=nid]
                     set_setting("notices", json.dumps(NOTICES)); error = "<div class=error>Notice Deleted</div>"
                 if "pin_notice" in request.form:
